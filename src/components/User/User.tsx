744e87c6fa8d289d145329567541f75690ae633b
Bar Almog <baralmog@protonmail.com>
2024-03-07 16:47:56 +0200

    Basic modal implemented

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -5,11 +5,11 @@ import TableCell from '@mui/material/TableCell';
 import { UsersDataProps } from '../../services/types';
 import formattedDate from '../../services/formattedDate';
 import { useListUsersContext } from '../../context/listUsersContext';
-import { Button, IconButton } from '@mui/material';
 import { AccountCircle } from '@material-ui/icons';
+import { NavLink } from 'react-router-dom';
 
 export default function User({ user }: { user: UsersDataProps }) {
-    const { setSelectedUser } = useListUsersContext()
+    // const { setSelectedUser } = useListUsersContext()
 
     return (
         <TableRow
@@ -20,7 +20,7 @@ export default function User({ user }: { user: UsersDataProps }) {
             </TableCell>
             <TableCell align="right">{user.gender.charAt(0).toUpperCase() + user.gender.slice(1)}</TableCell>
             <TableCell align="right">{formattedDate(user.dob.date)}</TableCell>
-            <TableCell align="right"><IconButton onClick={() => { setSelectedUser(user.login.uuid) }}><AccountCircle /></IconButton> </TableCell>
+            <TableCell align="right"><NavLink to={user.login.uuid}><AccountCircle /></NavLink> </TableCell>
         </TableRow>
     );
 }
--- src/components/UserModal/UserModal.tsx
+++ src/components/UserModal/UserModal.tsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import { Modal, Box, Typography, Divider, Button, Avatar } from '@mui/material';
+import { useParams, useHistory } from 'react-router-dom';
+
+import { useListUsersContext } from '../../context/listUsersContext';
+import formattedDate from '../../services/formattedDate';
 
 export default function UserModal() {
+    const { usersData } = useListUsersContext();
+    const { id } = useParams<{ id: string }>();
+    const user = usersData.find(user => user.login.uuid === id);
+    const history = useHistory();
+
+    // Render the modal only if user is defined
+    if (!user) {
+        return null;
+    }
+
+    const handleClose = () => {
+        // Remove the id parameter from the URL and navigate back
+        history.push('/');
+    };
+
     return (
-        <div>UserModal</div>
-    )
+        <Modal
+            open
+            onClose={handleClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Box sx={{
+                bgcolor: 'background.paper',
+                boxShadow: 1,
+                p: 4,
+                borderRadius: 2,
+                width: 600,
+            }}>
+                <Avatar
+                    alt={user.name.first + " " + user.name.last}
+                    src={user.picture.large}
+                    sx={{ width: 100, height: 100, mx: 'auto', mb: 2 }}
+                />
+                <Typography variant="h6" id="modal-title">
+                    Details for ID: {id}
+                </Typography>
+                <Divider />
+                <Typography variant="body1" id="modal-description">
+                    <strong>Full Name:</strong> {`${user.name.first} ${user.name.last}`}
+                </Typography>
+                <Typography variant="body1">
+                    <strong>Email:</strong> {user.email}
+                </Typography>
+                <Typography variant="body1">
+                    <strong>Gender:</strong> {user.gender}
+                </Typography>
+                <Typography variant="body1">
+                    <strong>Date of Birth:</strong> {formattedDate(user.dob.date)}
+                </Typography>
+                <Typography variant="body1">
+                    <strong>Nationality:</strong> {user.nat}
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button variant="contained" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Box>
+            </Box>
+        </Modal>
+    );
 }
